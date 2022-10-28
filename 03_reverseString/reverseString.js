3abf6533a2df3bf99fa1445c5619894e96bc382c
safried <112030532+safried@users.noreply.github.com>
2022-10-28 13:07:54 -0400

    add code to reverse single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = "";
+    let inputStringLength = inputString.length;
+    for (let i = (inputStringLength-1); i >= 0; i--){
+        outputString += inputString[i];
+    }
+    return outputString;
 };
 
 // Do not edit below this line
