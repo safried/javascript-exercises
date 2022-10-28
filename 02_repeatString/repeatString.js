99b366e80c0b3fe2051e0809178957f583882a17
safried <112030532+safried@users.noreply.github.com>
2022-10-28 11:35:13 -0400

    add code to repeat inputted string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, inputNumber) {
+    let newString = inputString;
+    for (let i = 1; i < inputNumber; i++){
+        newString += inputString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
