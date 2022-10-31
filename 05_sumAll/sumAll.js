67964e5d4d63d27d8a40fc26947e51510b20edb8
safried <112030532+safried@users.noreply.github.com>
2022-10-31 14:23:52 -0400

    add code to sum all numbers within range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
 
+    for (let i = 0; i < numTwo; i++){
+        sum += (numOne + i);
+    }
+    return sum;
+    
 };
+
+//for testing
+//sumAll(1,4);
 
 // Do not edit below this line
 module.exports = sumAll;
