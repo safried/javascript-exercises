5174b05071e7d659a13c6930b8b61242a690875e
safried <112030532+safried@users.noreply.github.com>
2022-10-31 15:24:38 -0400

    add preliminary code for both functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,23 @@
-const ftoc = function() {
+const ftoc = function(inputDegrees) {
+//x Fahrenheit = ((x-32) + (5/9)) Celsius
+  let outputDegrees;
+  outputDegrees = ((inputDegrees - 32) * (5/9));
+  outputDegrees = parseFloat(outputDegrees.toFixed(1));
+  return outputDegrees;
 
 };
 
-const ctof = function() {
-
+const ctof = function(inputDegrees) {
+//x Celsius = (x * (9/5) + 32) Fahrenheit
+  let outputDegrees;
+  outputDegrees = (inputDegrees * (9/5) + 32);
+  outputDegrees = parseFloat(outputDegrees.toFixed(1));
+  return outputDegrees;
 };
+
+
+//test
+//ftoc(32);
 
 // Do not edit below this line
 module.exports = {
