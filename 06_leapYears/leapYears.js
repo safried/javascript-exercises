5e461fde4ffbc48f9dcf7d532cacc85047723911
safried <112030532+safried@users.noreply.github.com>
2022-10-31 15:08:06 -0400

    code passes all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,14 +1,10 @@
 const leapYears = function(yearToTest) {
-    //if(yearToTest%100 == 0 && yearToTest%400 == 0){
-    //    return true;
-    
     if(yearToTest%100 == 0){
         if(yearToTest%400 == 0){
             return true;
         }else{
             return false;
         }
-    
     }else if (yearToTest%4 == 0){
         return true;
     }else{
