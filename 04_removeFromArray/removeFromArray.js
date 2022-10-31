75d9784726889d7fe3abbde8efd9b8d0b792af68
safried <112030532+safried@users.noreply.github.com>
2022-10-31 12:11:45 -0400

    write code to remove a single value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    let newArray = [];
+    let inputArrayLength = inputArray.length;
 
-};
+    for (let i = 0; i < inputArrayLength; i++){
+        if (inputArray[i] == elementsToRemove){
+            continue;
+        }else {
+            newArray.push(inputArray[i]);
+        }
+    }
+    return newArray;
+}
+
+//to aid in debugging
+//removeFromArray([1, 2, 3, 4], 3);
+//console.log(newArray);
 
 // Do not edit below this line
 module.exports = removeFromArray;
