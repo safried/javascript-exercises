5db793009c6df35724b94d43ade8b8f33f26a58e
safried <112030532+safried@users.noreply.github.com>
2022-10-31 15:03:01 -0400

    add code to determine leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(yearToTest) {
+    //let boolLeapYearValue;
 
+    if(yearToTest%100 == 0 && yearToTest%400 == 0){
+        return true;
+    }else if (yearToTest%4 == 0){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
